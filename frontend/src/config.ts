91fc13734932e154529fcab8519e0d2a9e7b3f4a
klaus-0-0 <jin0667666@gmail.com>
2025-08-10 04:16:26 +0530

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,5 @@
 const config = {
-    apiUrl: "https://chatapp-wj9f.onrender.com/api"
-    // apiUrl: "https://notes-1v6a.onrender.com/api"
+    apiUrl: "https://notes-1v6a.onrender.com/api"
 
 }
 export default config;
