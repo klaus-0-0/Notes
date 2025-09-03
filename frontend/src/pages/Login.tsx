c3bcb4633f513e32ca96a7f698335486291471c9
klaus-0-0 <jin0667666@gmail.com>
2025-09-03 15:22:41 +0530

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import config from "../config";
+// import config from "../config";
 import wall from "../assets/loginBI.png"
 
 interface LoginResponse {
