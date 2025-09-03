61279de972164df5f8b6382c1c7a3897c905b9fe
klaus-0-0 <jin0667666@gmail.com>
2025-09-03 14:56:56 +0530

    Delete backend/prisma/migrations/20250729230841_init directory

--- backend/prisma/migrations/20250729230841_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `title` on the `Note` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Note" DROP COLUMN "title";
