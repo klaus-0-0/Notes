a7e1abb1591670f7b32947bc29dd703ea35fd8be
klaus-0-0 <jin0667666@gmail.com>
2025-09-03 14:00:30 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://notes-frontend-c3jx.onrender.com",
   credentials: true,
   methods: ["GET", "POST"]
 }))
