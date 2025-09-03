a2c3003f927e579ce5755291885809ecb893b9b2
klaus-0-0 <jin0667666@gmail.com>
2025-09-03 14:00:48 +0530

    Update index.js

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,7 +4,7 @@ import auth from './auth.js';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://notes-frontend-c3jx.onrender.com",
     credentials: true,
     methods: ["GET", "POST"]
 }));
